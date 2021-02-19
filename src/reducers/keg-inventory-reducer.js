d6d3fbd6f2a6451e5b1685da8876d9229da3f383
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-02-19 09:26:37 -0800

    Purposefully passed test of addition of keg to kegInventory.

--- src/reducers/keg-inventory-reducer.js
+++ src/reducers/keg-inventory-reducer.js
@@ -1,3 +1,17 @@
 export default (state = {}, action) => {
-  return state;
-}
+  const { name, brand, alcCont, price, id} = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id] : {
+          name,
+          brand,
+          alcCont,
+          price,
+          id
+        }
+      });
+      default:
+      return state;
+  }
+};
