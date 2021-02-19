a75ce5736921f1cd658170ec591ade443ae7c308
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-02-19 09:19:44 -0800

    Purposefully failed test of keg-inventory-reducer to add keg to kegInventory.

--- src/__tests__/reducers/keg-inventory-reducer.test.js
+++ src/__tests__/reducers/keg-inventory-reducer.test.js
@@ -1,8 +1,37 @@
 import kegInventoryReducer from '../../reducers/keg-inventory-reducer';
 
 describe('kegInventoryReducer', () => {
+  let action;
+  const kegInfo = {
+    name: 'HTMLager',
+    brand: 'CSS Brewing',
+    alcCont: '4.5',
+    price: '6.00',
+    id: 1
+
+  }
   test('Should return default state if there is no action passed into the reducer', () => {
     expect(kegInventoryReducer({}, {type: null})).toEqual({});
   });
+  test('Should successfully add new keg data to kegInventory', () => {
+    const {name, brand, alcCont, price, id} = kegInfo;
+    action = {
+      type: 'ADD_KEG',
+      name,
+      brand,
+      alcCont,
+      price,
+      id
+    }
+    expect(kegInventoryReducer({}, action)).toEqual({
+      [id] : {
+        name,
+        brand,
+        alcCont,
+        price,
+        id
+      }
+    })
+  })
 });
 
